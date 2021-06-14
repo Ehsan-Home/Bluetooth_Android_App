5b25e5aa1be9dfc06558ffd32ce84695bd7623ba
ehsanghasaei <ehsan.ghasaei@gmail.com>
2021-06-14 12:16:17 -0700

    Bluetooth data instance class created

---
+++ app/src/main/java/com/example/bluetoothapp/BluetoothDataInstance.java
@@ -0,0 +1,11 @@
+package com.example.bluetoothapp;
+
+public class BluetoothDataInstance {
+    private String name;
+
+    BluetoothDataInstance(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name;}
+}
